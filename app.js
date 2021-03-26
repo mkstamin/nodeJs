911878aa8754f37b75644efd8d64a936696e0f1b
mkstamin <mkstamincse@gmail.com>
2021-03-26 17:34:34 +0600

    cheange

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({ message: 'Hello from the server side', app: 'my app' });
-// });
-
-// app.get('/', (req, res) => {
-//     res.send('HELLO FROM THE POST ROUTE');
-// });
-
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
