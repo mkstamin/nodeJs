d73bb038b6edea353a7fd3ec4acdc1aa33c4f237
mkstamin <mkstamincse@gmail.com>
2021-03-26 17:30:39 +0600

    read data from the local file and add a new route /api/v1/tours

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello from the server side', app: 'my app' });
-});
+// app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Hello from the server side', app: 'my app' });
+// });
 
-app.get('/', (req, res) => {
-    res.send('HELLO FROM THE POST ROUTE');
+// app.get('/', (req, res) => {
+//     res.send('HELLO FROM THE POST ROUTE');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours,
+        },
+    });
 });
 
 const port = 3000;
