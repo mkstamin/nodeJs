6dee69684e6b8a3553b7cb2a4bb059eae6992097
mkstamin <mkstamincse@gmail.com>
2021-04-29 15:29:53 +0600

    upload file using MULTER and SHARP

--- public/js/index.js
+++ public/js/index.js
@@ -38,10 +38,12 @@ if(userDataForm){
     userDataForm.addEventListener('submit', (e) => {
         e.preventDefault();
         
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        
-        updateSettings({name, email}, 'data');
+        const form = new FormData(); // it is used instance of  enctype="multipart/form-data"
+        form.append('name', document.getElementById('name').value);
+        form.append('email', document.getElementById('email').value);
+        form.append('photo', document.getElementById('photo').files[0]);
+
+        updateSettings(form, 'data');
     });  
 }
 
--- public/js/updateSetting.js
+++ public/js/updateSetting.js
@@ -17,6 +17,7 @@ export const updateSettings = async (data, type) => {
 
         if (res.data.status === 'success') {
             showAlert('success', `${type.toUpperCase()} update successfully`);
+            location.reload(true)
         }
     } catch (err) {
         showAlert('error', err.response.data.message);
