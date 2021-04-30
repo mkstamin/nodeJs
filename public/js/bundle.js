3da9b3918c04127b978e54e7128c7d557e872b61
mkstamin <mkstamincse@gmail.com>
2021-04-30 16:49:59 +0600

    Email Templates with Pug Welcome Emails

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9075,7 +9075,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51659" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51080" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- utils/email.js
+++ utils/email.js
@@ -1,25 +1,49 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-    // create a transpoter
-    const transpoter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    });
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        const [firstName] = user.name.split(' ')[0];
+        this.firstName = firstName;
+        this.url = url;
+        this.from = `Mks Tamin <${process.env.EMAIL_FROM}>`;
+    }
 
-    // Define the email options
-    const mailOptions = {
-        from: 'Mks Tamin <admin@mks.me>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    };
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            return this.firstName;
+        }
 
-    await transpoter.sendMail(mailOptions);
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+    }
+
+    async send(template, subject) {
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject,
+        });
+
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html),
+        };
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.send('welcome', 'Welcome to the Naturs Family!');
+    }
 };
-
-module.exports = sendEmail;
